1cc84b8a4ae49670263c3ee1965f65ae970c22ef
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-11-08 19:08:40 +0530

    program

---
+++ welcome.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	//clrscr();
+	printf(" Welcome to Ssi Digital ");
+	getch();
+	printf("\n This is C/C++ Class , Bhawarkuam Indore");
+	getch();
+	printf("\n 452001");
+	getch();
+}
